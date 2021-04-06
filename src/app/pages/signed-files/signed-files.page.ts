934cf64886c508c4102481a6486d93d2454cf4b0
augusv123 <augustovidal94@gmail.com>
2021-04-06 15:52:09 -0300

    cambios proceso de visualizacion

--- src/app/pages/signed-files/signed-files.page.ts
+++ src/app/pages/signed-files/signed-files.page.ts
@@ -25,7 +25,11 @@ export class SignedFilesPage implements OnInit {
     const id = this.route.snapshot.paramMap.get('id');
     const nombre = this.route.snapshot.paramMap.get('nombre');
     const carpeta = this.route.snapshot.paramMap.get('carpeta');
-    console.log(id+"/"+carpeta+"/"+nombre)
+    var path = id+"/"+carpeta+"/"+nombre
+    if(id && nombre && carpeta){
+      this.getSignedForm(path)
+
+    }
     // if(fulldirectory){
     //   this.getSignedForm(fulldirectory)
     // }
